1051beca6a41c5112645f7e390e4e63dd6288e64
sigoden <sigoden@gmail.com>
2024-07-12 19:28:10 +0800

    feat: add coder agent (#70)

---
+++ agents/coder/README.md
@@ -0,0 +1,20 @@
+# Coder
+
+Assist your coding tasks.
+
+## Features
+
+- 🏗️ Intelligent project structure creation and management
+- 🖼️ Convert screenshots into clean, functional code
+- 📁 Comprehensive file system operations (create folders, files, read/write files)
+- 🧐 Advanced code analysis and improvement suggestions
+- 📊 Precise diff-based file editing for controlled code modifications
+- 🔍 Web search capabilities for up-to-date information
+
+## Examples
+
+![image](https://github.com/user-attachments/assets/ad9c9d94-5248-46ba-be30-cc885bda9333)
+
+https://github.com/user-attachments/assets/0851b9e7-3ac5-4ec1-ab8b-d845c460446b
+
+https://github.com/user-attachments/assets/df459938-3abf-442b-bc51-42bdb9c72c43
---
+++ agents/coder/tools.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+set -e
+
+# @env FS_BASE_DIR=. The base dir
+
+# @cmd Create a new file at the specified path with content.
+# @option --path! The path where the file should be created
+# @option --content! The content of the file
+fs_create() {
+    path="$FS_BASE_DIR/$argc_path"
+    printf "%s" "$argc_content" > "$path"
+    echo "File created: $path" >> "$LLM_OUTPUT"
+}
+
+# @cmd Apply changes to a file.  Use this when you need to edit an existing file.
+# YOU ALWAYS PROVIDE THE FULL FILE CONTENT WHEN EDITING. NO PARTIAL CONTENT OR COMMENTS.
+# YOU MUST PROVIDE THE FULL FILE CONTENT.
+
+# @option --path! The path of the file to edit
+# @option --content! The new content to apply to the file
+# @meta require-tools git
+fs_edit() {
+    path="$FS_BASE_DIR/$argc_path"
+    if [[ -f "$path" ]]; then
+        changed=0
+        printf "%s" "$argc_content" | git diff --no-index "$path" - || {
+            changed=1
+        }
+        if [[ "$changed" -eq 0 ]]; then
+            echo "No changes detected." >> "$LLM_OUTPUT"
+        else
+            if [ -t 1 ]; then
+                echo
+                read -r -p "Apply changes? [Y/n] " ans
+                if [[ "$ans" == "N" || "$ans" == "n" ]]; then
+                    echo "Aborted!"
+                    exit 1
+                fi
+            fi
+            printf "%s" "$argc_content" > "$path"
+            echo "Applied changes" >> "$LLM_OUTPUT"
+        fi
+    else
+        echo "Not found file: $path" >> "$LLM_OUTPUT"
+    fi
+}
+
+# See more details at https://github.com/sigoden/argc
+eval "$(argc --argc-eval "$0" "$@")"
