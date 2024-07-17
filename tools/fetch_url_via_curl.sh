4d18d6346d4aea16cc9dac1bcfc3ccc4c15a7bd0
sigoden <sigoden@gmail.com>
2024-07-17 18:29:41 +0800

    refactor: improve fetch_url_via_curl (#77)

--- tools/fetch_url_via_curl.sh
+++ tools/fetch_url_via_curl.sh
@@ -8,8 +8,8 @@ set -e
 main() {
     # span and div tags are dropped from the HTML https://pandoc.org/MANUAL.html#raw-htmltex and sed removes any inline SVG images in image tags from the Markdown content.
     curl -fsSL "$argc_url" | \
-        pandoc -f html-native_divs-native_spans -t gfm-raw_html | \
-        sed -E 's/!\[.*?\]\((data:image\/svg\+xml[^)]+)\)//g' \
+        pandoc -f html-native_divs-native_spans -t gfm-raw_html --wrap=none | \
+        sed -E 's/!\[[^]]*\]\([^)]*\)//g' \
         >> "$LLM_OUTPUT"
 }
 
