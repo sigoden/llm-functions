c43aafc4919615b102748ff2689bfccacf3cce36
sigoden <sigoden@gmail.com>
2024-08-02 15:07:56 +0000

    feat: prompt confirmation for fs write operations outside cwd

--- tools/fs_cat.sh
+++ tools/fs_cat.sh
@@ -4,12 +4,10 @@ set -e
 # @describe Read the contents of a file at the specified path.
 # Use this when you need to examine the contents of an existing file.
 
-# @env FS_BASE_DIR=. The base dir
 # @option --path! The path of the file to read
 
 main() {
-    path="$FS_BASE_DIR/$argc_path"
-    cat "$path" >> "$LLM_OUTPUT"
+    cat "$argc_path" >> "$LLM_OUTPUT"
 }
 
 eval "$(argc --argc-eval "$0" "$@")"
--- tools/fs_mkdir.sh
+++ tools/fs_mkdir.sh
@@ -3,13 +3,11 @@ set -e
 
 # @describe Create a new directory at the specified path.
 
-# @env FS_BASE_DIR=. The base dir
 # @option --path! The path of the directory to create
 
 main() {
-    path="$FS_BASE_DIR/$argc_path"
-    mkdir -p "$path"
-    echo "Directory created: $path" >> "$LLM_OUTPUT"
+    mkdir -p "$argc_path"
+    echo "Directory created: $argc_path" >> "$LLM_OUTPUT"
 }
 
 eval "$(argc --argc-eval "$0" "$@")"
